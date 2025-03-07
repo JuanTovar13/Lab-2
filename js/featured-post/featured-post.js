01a83ae8a20e0c358d4f264ccbd2c9883d0d1bb7
Zaphkiel2200 <135277854+Zaphkiel2200@users.noreply.github.com>
2025-03-07 18:13:10 -0500

    add index

--- js/featured-post/featured-post.js
+++ js/featured-post/featured-post.js
@@ -1,1 +1,8 @@
-class FeaturedPost 
+class FeaturedPost extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <h2>Featured Post</h2>
+      <p>This is a featured post.</p>
+    `;
+  }
+}
