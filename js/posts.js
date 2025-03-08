3f2d73ebbf92e521968da2f96864e7d8e01a5e72
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-03-08 17:19:55 -0500

    add post.js

---
+++ js/posts.js
@@ -0,0 +1,24 @@
+const posts = [
+    {
+      image: "assets/images/post1.jpg",
+      title: "Post 1 Title",
+      date: "January 1, 2023",
+      description: "This is the first post description...",
+      link: "/post/1"
+    },
+    {
+      image: "assets/images/post2.jpg",
+      title: "Post 2 Title",
+      date: "February 15, 2023",
+      description: "This is the second post description...",
+      link: "/post/2"
+    },
+    {
+      image: "assets/images/post3.jpg",
+      title: "Post 3 Title",
+      date: "March 30, 2023",
+      description: "This is the third post description...",
+      link: "/post/3"
+    }
+  ];
+  
