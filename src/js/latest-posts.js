6597b98fcd8ff5df80fd32451c4f88f81089b989
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-03-13 11:13:13 -0500

    add latest-post & restoring the file

---
+++ src/js/latest-posts.js
@@ -0,0 +1,16 @@
+import posts from './posts.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const latestSection = document.getElementById('latest-posts');
+
+    posts.forEach(post => {
+        const postCard = document.createElement('post-card');
+        postCard.setAttribute('title', post.title);
+        postCard.setAttribute('category', post.category);
+        postCard.setAttribute('author', post.authors.join(', ')); 
+        postCard.setAttribute('date', post.date);
+        postCard.setAttribute('content', post.description); 
+
+        latestSection.appendChild(postCard);
+    });
+});
