6a6631e2a7025176cd290392bac8c5e312a9f72c
unknown <juanstovarm@gmail.com>
2025-03-13 21:27:59 -0500

    index1.2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+
+
 import HeaderComponent from './components/header/header.js';
 import SearchComponent from './components/search/search.js';
+import CardCustom from './components/card/card.js';
+import ListCardsComponent from './components/card/ListCardsComponent.js';
+
+
 
 console.log("Custom element defined");
 customElements.define('header-component', HeaderComponent);
 customElements.define('search-component', SearchComponent);
+
+customElements.define('card-list', ListCardsComponent);
+customElements.define('custom-card', CardCustom);
 
 
