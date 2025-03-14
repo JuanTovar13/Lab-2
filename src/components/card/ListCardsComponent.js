b1b77c13e89afd5e6a8330a5c34e6c5363231afa
unknown <juanstovarm@gmail.com>
2025-03-13 21:32:18 -0500

    cardList1.0

---
+++ src/components/card/ListCardsComponent.js
@@ -0,0 +1,49 @@
+class ListCardsComponent extends HTMLElement  {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    async connectedCallback() {
+        try {
+            const response = await fetch('./src/components/card/card.json');
+            const CardRen = await response.json();
+            const cards = CardRen.cardJson;
+
+            this.shadowRoot.innerHTML = `
+            <style>
+        .card-container {
+            display: grid;
+            grid-template-columns: repeat(2, 3fr); 
+            gap: 15px; /* Espaciado entre las cards */
+            width: 100%;
+            max-width: 1200px; /* Límite de ancho */
+            
+            padding: 20px;
+            
+        }
+            .card:nth-child(3),
+            .card:nth-child(6) {
+    grid-column: span 2;
+    
+}
+
+        
+    </style>
+            <div class="card-container">
+                ${cards.map(card => `
+                
+                    <custom-card class="card${card.image ? '' : 'no-image'}" id="${card.id}" category="${card.category}" title="${card.title}" description="${card.description}" image="${card.image}" author="${card.author}" date="${card.date}"></custom-card>
+                
+                `).join('')}
+
+            </div>
+            `;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+}
+
+export default ListCardsComponent;
